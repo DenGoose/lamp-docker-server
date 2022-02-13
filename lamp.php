d89a6074ac71bf7173972d2dfa5d273783676009
Гусельников Денис <guselnikov@intervolga.ru>
2022-02-13 03:09:27 +0300

    Добавил readme.md и поправил lamp.php

--- lamp.php
+++ lamp.php
@@ -4,6 +4,16 @@
 $_SERVER['DOCUMENT_ROOT'] = __DIR__;
 const SERVER_MAIN_PROXY = 'proxy';
 
+global $wsl;
+$wsl = '';
+
+$isWin = shell_exec("uname -s 2>&1");
+
+if (!stristr($isWin, 'Linux') || stristr($isWin, 'cmdlet') || stristr($isWin, '"uname"'))
+{
+	$wsl = 'wsl ';
+}
+
 $command = $argv[1] ?? '';
 $server = $argv[2] ?? '';
 
@@ -26,11 +36,13 @@ function createEnv(string $path, array $ar): void
 
 function getServersList(): array
 {
+	global $wsl;
+
 	$dirList = array_filter(scandir($_SERVER['DOCUMENT_ROOT'] . '/sites/'), function($itm) {
 		return (($itm != '.') && ($itm != '..') && file_exists($_SERVER['DOCUMENT_ROOT'] . '/sites/' . $itm . '/.env'));
 	});
 
-	$str = shell_exec('wsl docker container ls -a --format=\'{{json .}}\' 2>&1; echo $?');
+	$str = shell_exec($wsl . 'docker container ls -a --format=\'{{json .}}\' 2>&1');
 
 	$arr = array_filter(explode("\n", $str), function ($itm)
 	{
@@ -75,7 +87,7 @@ function server(string $server, string $command)
 {
 	if (strlen($server) && strlen($command))
 	{
-		createEnv("\\sites\\${server}\\.env", ["APP_NAME=${server}"]);
+		createEnv("/sites/${server}/.env", ["APP_NAME=${server}"]);
 		shell_exec("docker-compose -f docker-compose.server.yml -p ${server} --env-file ./sites/${server}/.env ${command}");
 	}
 }
@@ -91,21 +103,21 @@ function deleteServerFiles(string $server)
 		}
 		proxy(command: 'down');
 
-		$str = str_replace('/', '', file_get_contents($_SERVER['DOCUMENT_ROOT'] . "\\.gitignore"));
+		$str = str_replace('/', '', file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/.gitignore"));
 		$dirs = array_filter(explode("\n", $str), function($itm){
 			return (trim($itm) != '.idea') && (trim($itm) != '*env');
 		});
 
 		foreach ($dirs as $dir)
 		{
-			delTree($_SERVER['DOCUMENT_ROOT'] . '\\' . $dir);
+			delTree($_SERVER['DOCUMENT_ROOT'] . '/' . $dir);
 		}
 	}
 	else
 	{
-		delTree($_SERVER['DOCUMENT_ROOT'] . '\\ext_www\\' . $server . "\\");
-		delTree($_SERVER['DOCUMENT_ROOT'] . '\\sites\\' . $server . "\\");
-		delTree($_SERVER['DOCUMENT_ROOT'] . '\\databases\\' . $server . "\\");
+		delTree($_SERVER['DOCUMENT_ROOT'] . "/ext_www/${server}/");
+		delTree($_SERVER['DOCUMENT_ROOT'] . "/sites/${server}/");
+		delTree($_SERVER['DOCUMENT_ROOT'] . "/databases/${server}/");
 	}
 }
 
@@ -119,7 +131,7 @@ function delTree($dir): bool
 
 		foreach ($files as $file)
 		{
-			(is_dir("$dir\\$file")) ? delTree("$dir\\$file") : unlink("$dir\\$file");
+			(is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file");
 		}
 		return rmdir($dir);
 	}
@@ -196,25 +208,33 @@ switch ($command)
 		break;
 	case 'list':
 		$info = getServersList();
-		$temp = $info[array_key_first($info)];
 
-		$serverList = array_keys($info);
-		$columns = array_keys($temp[array_key_first($temp)]);
-		echo "\n";
-		$mask = "|%-30.30s |%-15.15s |%-15.15s |%-30.30s |%-15.15s |\n";
-
-		foreach ($serverList as $item)
+		if ($info)
 		{
-			echo "Server: \033[0;31m${item}\033[0m\n\n";
+			$temp = $info[array_key_first($info)];
 
-			$count = printf($mask, ...$columns);
-			echo str_repeat('-', $count) . "\n";
-
-			foreach ($info[$item] as $containerName => $containerValues)
-			{
-				printf($mask, ...array_values($containerValues));
-			}
+			$serverList = array_keys($info);
+			$columns = array_keys($temp[array_key_first($temp)]);
 			echo "\n";
+			$mask = "|%-30.30s |%-15.15s |%-15.15s |%-30.30s |%-15.15s |\n";
+
+			foreach ($serverList as $item)
+			{
+				echo "Server: \033[0;31m${item}\033[0m\n\n";
+
+				$count = printf($mask, ...$columns);
+				echo str_repeat('-', $count) . "\n";
+
+				foreach ($info[$item] as $containerName => $containerValues)
+				{
+					printf($mask, ...array_values($containerValues));
+				}
+				echo "\n";
+			}
+		}
+		else
+		{
+			echo "0 servers\n";
 		}
 
 		break;
---
+++ readme.md
@@ -0,0 +1,31 @@
+# lamp
+
+### Запуск прокси сервера
+
+```bash
+php lamp.php up proxy
+```
+
+### Создание серверов
+
+```bash
+php lamp.php up [имя сервера]
+```
+
+### Остановка сервера
+
+```bash
+php lamp.php stop [имя сервера]
+```
+
+### Удаление сервера
+
+```bash
+php lamp.php delete [имя сервера]
+```
+
+Список серверов
+
+```bash
+php lamp.php list
+```
